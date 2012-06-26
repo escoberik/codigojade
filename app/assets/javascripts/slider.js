fbcc194bb72c8717b7991f652acff27fc0415a61
Huascar Ona <huascar@codigojade.com>
2012-06-26 12:50:09 -0500

    agregado contenido en homepage y eyecatcher

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -4,6 +4,51 @@
 
 $(document).ready(function() {
 
-
-
-}
+  var $high = $('.highquality');
+  var $software = $('.software');
+  var $web = $('.web');
+  var $contentman = $('.contentman');
+  var $continuum = $('.continuum');
+  
+  $('.first').hover(
+    function () { 
+      $software.removeClass("none");
+      $high.addClass("none");
+    },
+    function () {
+      $software.addClass("none");
+      $high.removeClass("none");
+    }
+  );
+  $('.second').hover(
+    function () { 
+      $web.removeClass("none");
+      $high.addClass("none");
+    },
+    function () {
+      $web.addClass("none");
+      $high.removeClass("none");
+    }
+  );
+  $('.third').hover(
+    function () { 
+      $contentman.removeClass("none");
+      $high.addClass("none");
+    },
+    function () {
+      $contentman.addClass("none");
+      $high.removeClass("none");
+    }
+  );
+  $('.last').hover(
+    function () { 
+      $continuum.removeClass("none");
+      $high.addClass("none");
+    },
+    function () {
+      $continuum.addClass("none");
+      $high.removeClass("none");
+    }
+  );
+  
+});
