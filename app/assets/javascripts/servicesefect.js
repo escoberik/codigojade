1350662afbd9d54939ce266e6c594efba8e451e9
Huascar Ona <huascar@codigojade.com>
2012-06-27 21:28:42 -0500

    footer y efecto imagenes

---
+++ app/assets/javascripts/servicesefect.js
@@ -0,0 +1,25 @@
+// = require jquery
+// = require jquery-ui
+// = require jquery_ujs
+
+$(document).ready(function() { 
+  $('.tds').hover(
+    function (e) {
+      var $img = $(e.target).find('img.none');
+      var $imgv = $(e.target).find('img.visible');
+      $img.removeClass("none");
+      $img.addClass("visible");
+      $imgv.removeClass("visible");
+      $imgv.addClass("none");
+
+    },
+    function (e) {
+      var $img = $(e.target).find('img.none');
+      var $imgv = $(e.target).find('img.visible');
+      $img.removeClass("none");
+      $img.addClass("visible");
+      $imgv.removeClass("visible");
+      $imgv.addClass("none");
+    }
+  );
+});
